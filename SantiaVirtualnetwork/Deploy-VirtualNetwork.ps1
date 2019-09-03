8736ccf5c9fbfdf86699b7fabe58570ed771b173
saddique <saddique_uet@yahoo.com>
2019-09-04 00:07:50 +0400

    first commit

---
+++ SantiaVirtualnetwork/Deploy-VirtualNetwork.ps1
@@ -0,0 +1,29 @@
+﻿{
+$resourceGroupName = 'SantiaDemo'
+$resourceGroupLocation = 'Southeast Asia'
+$resoruceDeploymentName= "SantiaVirtualNetwork"
+$templatePath = $env:SystemDrive + '\' + 'santia\SantiaVirtualnetwork\SantiaVirtualnetwork'
+$templateName= 'azuredeploy.json'
+$template = $templatePath + '\' + $templateName
+}
+
+
+# Creating a new Resource Group
+
+{
+New-AzureRmResourceGroup `
+  -Location $resourceGroupLocation `
+  -Name $resourceGroupName `
+  -verbose -Force
+}
+
+# Creation of Virtual Network
+
+{
+ New-AzureRmResourceGroupDeployment `
+  -Name $resoruceDeploymentName `
+  -ResourceGroupName $resourceGroupName `
+  -TemplateFile $template `
+  -verbose -Force
+
+}
